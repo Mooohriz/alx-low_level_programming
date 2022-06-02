704bf82f6a085e92350331d5484a9a351dc582a8
Mooohriz <mohammadyekini@gmail.com>
2022-06-02 13:11:50 -0700

    flex

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,6 +8,8 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")		
-   return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }	
+
