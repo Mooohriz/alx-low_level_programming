bb7d55a4e809acb730dcd552879f66e0d9e9af22
Mooohriz <mohammadyekini@gmail.com>
2022-06-02 12:52:57 -0700

    eden

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-main entry - prints phrase with proper grammar, but the outcome is a piece of art,
-
-Return: Always 0 (Success)
-*/
+ *main entry - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
