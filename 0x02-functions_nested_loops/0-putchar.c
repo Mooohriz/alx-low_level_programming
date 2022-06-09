5a625adaa36b23b94c5134666839ad3b7dbc63f1
Mooohriz <mohammadyekini@gmail.com>
2022-06-09 05:36:37 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * main A program that prints _putchar
+ * main - A program that prints _putchar
  * Return: 0 as success
  */
 int main(void)
 {
 	char a[] = "_putchar";
-	
 	int c;
 	
-	for (c = 0; c < 8; c++)
+for (c = 0; c < 8; c++)
 	{
 		_putchar(a[c]);
 	}
