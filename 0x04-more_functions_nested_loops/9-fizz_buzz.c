8ba2f316605301a863186369e394c86dcea903d2
Mooohriz <mohammadyekini@gmail.com>
2022-06-09 18:56:37 -0700

    esse

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,8 +14,8 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if(i % 5 == 0)
-			printf("buzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
