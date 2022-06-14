84e3a966a24bb196965f33873acf5884f622c3de
Mooohriz <mohammadyekini@gmail.com>
2022-06-14 02:07:09 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - prints a string followed by a new line
+*@str: string to print
+*Return: nothing
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + 1) != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
