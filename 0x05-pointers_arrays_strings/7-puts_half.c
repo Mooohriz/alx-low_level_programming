fb9113e741ebd180568b1dffe32bff932dd0e0b0
Mooohriz <mohammadyekini@gmail.com>
2022-06-14 03:18:11 +0100

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,7 +25,7 @@ for (i = len / 2; str[i] != '\0'; i++)
 }
 else if (len % 2)
 {
-for (n = (len - 1) / 2; n < len - 1; n++;}
+for (n = (len - 1) / 2; n < len - 1; n++);
 {	_putchar(str[n + 1]);
 }
 }
