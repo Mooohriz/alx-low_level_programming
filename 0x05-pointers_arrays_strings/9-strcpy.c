d1c96862c46cae0ed528030e84b5d895a1db4776
Mooohriz <mohammadyekini@gmail.com>
2022-06-14 04:03:42 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i = -1;
-do {
-i++;
-dest[i] = src[i];
-} while (src[i] != '\0');
+int i;
 
-return (dest);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
