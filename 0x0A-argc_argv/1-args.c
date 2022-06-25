1280fd776da7aba92b55d3a1da6f1c54edcc366b
Mooohriz <mohammadyekini@gmail.com>
2022-06-25 02:45:41 +0100

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - ptints the for the program
+*@argc: is a counter for argument for int
+*@argv: is a counter fir value of the argument to the char
+*Return: returns always success
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", (argc - 1));
+
+return (0);
+}
+
