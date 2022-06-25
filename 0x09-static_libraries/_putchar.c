f3bc40d8f520f0e1a625fc55bcbd70f40bcb9673
Mooohriz <mohammadyekini@gmail.com>
2022-06-25 01:20:52 +0100

    _putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+/**
+ * main - A program that prints _putchar
+ * Return: 0 as success
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int c;	
+for (c = 0; c < 8; c++)
+{
+		_putchar(a[c]);
+	}
+			_putchar('\n');
+	return (0);
+}
+
