2cb458cf21b0366a168a91d3da1f085972c58176
Mooohriz <mohammadyekini@gmail.com>
2022-06-27 23:54:33 +0100

    string_concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int size1 = 0, size2 = 0;
 	char *ptr, *ret;
 	ptr = s1;
+
 	if (s1)
 		while (*ptr++)
 			size1++;
@@ -25,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 		while (*ptr++)
 			size2++;
 	else
-		s2 = "";
+		s2 = " ";
 	ret = malloc(size1 + size2 + 1);
 	if (!ret)
 		return (NULL);
